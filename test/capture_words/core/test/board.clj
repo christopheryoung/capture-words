df49025ffbad7408348820445e7df1feafb5ad7f
Christopher Young <chris@chrisyoung.net>
2012-04-20 11:39:59 -0400

    Bumped to clojure 1.4

--- test/capture_words/core/test/board.clj
+++ test/capture_words/core/test/board.clj
@@ -1,6 +1,6 @@
 
 (ns capture_words.core.test.board
-  (:use [capture_words.core.board :only (adjacent?)])
+  (:use [capture_words.core.board])
   (:use [clojure.test]))
 
 (deftest test-adjacent?
@@ -9,7 +9,10 @@
   (is (adjacent? [[1 1] [2 1]]))
   (is (not (adjacent? [[1 1] [3 1]]))))
 
-
+(deftest test-change-tile-value?
+  (let [board (init-board)]
+    (change-tile-value board [1 1] {:player :blue})
+    (is (get-tile board [1 1]) {:player :blue :letter nil})))
 
 (run-all-tests)
 
