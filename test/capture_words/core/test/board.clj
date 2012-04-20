9a35dc45575ed787c690b4c46aa9fcf929ad7497
Christopher Young <chris@chrisyoung.net>
2012-04-20 16:31:39 -0400

    Added test for change tile defn

--- test/capture_words/core/test/board.clj
+++ test/capture_words/core/test/board.clj
@@ -9,10 +9,11 @@
   (is (adjacent? [[1 1] [2 1]]))
   (is (not (adjacent? [[1 1] [3 1]]))))
 
-(deftest test-change-tile-value?
-  (let [board (init-board)]
-    (change-tile-value board [1 1] {:player :blue})
-    (is (get-tile board [1 1]) {:player :blue :letter nil})))
+(deftest test-change-tile-value
+  (let [test-board (init-board)]
+    (is (= (get-tile test-board [1 1]) {:player nil :letter nil}))
+    (change-tile-value test-board [1 1] {:player :test-passer})
+    (is (= (get-tile test-board [1 1]) {:player :test-passer :letter nil}))))
 
 (run-all-tests)
 
