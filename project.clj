af1086dcfe070372ad1b663a28feda34256dbd30
Christopher Young <chris@chrisyoung.net>
2012-04-23 11:03:50 -0400

    Deleting lein.swank plugin from project (and moved to personal .lein)

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
-                 [org.clojure/math.combinatorics "0.0.2"]]
-  :plugins [[lein-swank "1.4.4"]])
+                 [org.clojure/math.combinatorics "0.0.2"]])
