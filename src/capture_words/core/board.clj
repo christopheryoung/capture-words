5bef89149582afb7b3862dcd453af30333a9ba4f
Christopher Young <chris@chrisyoung.net>
2012-04-23 11:05:09 -0400

    Refactored board so it's just a map and not an atom any more. Updated helper function and make-board funcs. Updated tests accordingly.

--- src/capture_words/core/board.clj
+++ src/capture_words/core/board.clj
@@ -7,57 +7,50 @@
   {:player nil
    :letter nil})
 
-(defn make-board [board-length board-width]
-  (atom (vec (for [x (range board-length)]
-         (vec (take board-width (repeatedly tile)))))))
-
 (defn legal-coordinates-for-board? [board]
   "Returns a function that takes a set of coordinates and returns true
   if they are legal on the board and false if they are not."
-  (fn [coordinates]
-    (try (get-in board coordinates)
-         true false)))
+  (fn [[x y]]
+    (try
+      ((board x) y)
+      true
+      (catch IndexOutOfBoundsException e false))))
 
-;;TODO: Assumes that the returned positions are legal.
-(defn neighbors? [[x y]]
+(defn neighbors-for-coordinates [board [x y]]
+  "Takes a board and a set of coordinates and returns all legal
+neighbors for those coordinates on the given board."
   (let [up [x (+ y 1)]
         right [(+ x 1) y]
         down [x (- y 1)]
-        left [(- x 1) y]]
-  [up right down left]))
+        left [(- x 1) y]
+        legal? (legal-coordinates-for-board? board)]
+    (filter legal? [up right down left])))
 
 (defn in-coll? [elem coll]
   (boolean (some #{elem} coll)))
 
-(defn adjacent? [[pair1 pair2]]
-  (in-coll? pair1 (neighbors? pair2)))
-
-(defn none-adjacent? [coll]
-  (not-any? adjacent? (combinations coll 2)))
-
-(defn random-letter []
-  (rand-nth (map char (range (int \A) (int \Z)))))
-
-(defn random-tile-coordinates [board-length board-width]
-  "Lazy seq of random tile coordinates"
-  (repeatedly #(vector (rand-int board-length) (rand-int board-width))))
+(defn neighbors? [board pair1 pair2]
+  "Takes a board and two coordinate pairs and returns true or false
+depending on whether the coordinate pairs are neighbors on the board."
+  (in-coll? pair1 (neighbors-for-coordinates board pair2)))
 
 (defn get-tile [board coordinates]
-  (get-in @board coordinates))
+  "Returns the tile at the given coordinates"
+  (get-in board coordinates))
 
 (defn change-tile-value [board coordinates updates]
+  "Returns a new board featuring the updates to the tile at the given
+coordinates."
   (let [old-tile (get-tile board coordinates)
         new-tile (merge old-tile updates)]
-    (swap! board assoc-in coordinates new-tile)))
+    (assoc-in board coordinates new-tile)))
 
-(defn init-board [ & {:keys [board-length board-width pre-fills]
+(defn make-board [ & {:keys [board-length board-width init-func tile-func]
                       :or {board-length 15
                            board-width 15
-                           pre-fills 4}}]
-  (let [board (make-board board-length board-width)
-        random-tile-groups (partition pre-fills (distinct (random-tile-coordinates board-length board-width)))
-        non-adjacent-random-tiles (first (filter none-adjacent? random-tile-groups))
-        ]
-    board))
-
-(def board (init-board))
+                           init-func identity
+                           tile-func tile}}]
+  "Returns a board using provided funcs"
+  (let [board (vec (for [x (range board-length)]
+                     (vec (take board-width (repeatedly tile-func)))))]
+    (init-func board)))
--- test/capture_words/core/test/board.clj
+++ test/capture_words/core/test/board.clj
@@ -3,17 +3,23 @@
   (:use [capture_words.core.board])
   (:use [clojure.test]))
 
-(deftest test-adjacent?
-  (is (adjacent? [[1 1] [1 2]]))
-  (is (not (adjacent? [[1 1] [1 3]])))
-  (is (adjacent? [[1 1] [2 1]]))
-  (is (not (adjacent? [[1 1] [3 1]]))))
+(deftest test-neighbors-for-coordinates?
+  (let [board (make-board)]
+    (is (= (neighbors-for-coordinates board [0 0])
+           [[0 1] [1 0]]))
+    (is (= (neighbors-for-coordinates board [4 4])
+           [[4 5] [5 4] [4 3] [3 4]]))))
+
+(deftest test-neighbors?
+  (let [board (make-board)]
+    (is (= (neighbors? board [0 0] [0 1]) true))
+    (is (= (neighbors? board [0 0] [5 5]) false))))
 
 (deftest test-change-tile-value
-  (let [test-board (init-board)]
-    (is (= (get-tile test-board [1 1]) {:player nil :letter nil}))
-    (change-tile-value test-board [1 1] {:player :test-passer})
-    (is (= (get-tile test-board [1 1]) {:player :test-passer :letter nil}))))
+  (let [board (make-board)
+        changed-board (change-tile-value board [1 1] {:player :test-passer})]
+    (is (= ((get-tile changed-board [1 1]) :player) :test-passer))))
+
 
 (run-all-tests)
 
