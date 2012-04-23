ba548f6c2f031f027c7cad0cc4de624e39522a57
Christopher Young <chris@chrisyoung.net>
2012-04-23 16:04:30 -0400

    Utilities for working with words

--- src/capture_words/word_utils/core.clj
+++ src/capture_words/word_utils/core.clj
@@ -1,21 +1,58 @@
 
 (ns capture_words.word_utils.core
-  (:import (java.io BufferedReader FileReader)))
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]))
 
-(defn get-dict [file-name]
-  )
+(defn letter-probability-map []
+  "Returns probabilities of letters based on frequencies in the language"
+  {"E" 12.02
+   "T" 9.10
+   "A" 8.12
+   "O" 7.68
+   "I" 7.31
+   "N" 6.95
+   "S" 6.28
+   "R" 6.02
+   "H" 5.92
+   "D" 4.32
+   "L" 3.98
+   "U" 2.88
+   "C" 2.71
+   "M" 2.61
+   "F" 2.30
+   "Y" 2.11
+   "W" 2.09
+   "G" 2.03
+   "P" 1.82
+   "B" 1.49
+   "V" 1.11
+   "K" 0.69
+   "X" 0.17
+   "Q" 0.11
+   "J" 0.10
+   "Z" 0.07})
 
-(defn- build-words [words word]
-  )
+(defn- upper-case-and-conj [coll x]
+  (conj coll (string/upper-case x)))
 
-(defn process-file [file-name line-func line-acc]
-  (let [word {}
-        ]
-    (with-open [rdr (BufferedReader. (FileReader. file-name))]
-      (reduce  (line-seq rdr))))
-  )
+(defn get-all-words [file-name]
+  (with-open [rdr (clojure.java.io/reader file-name)]
+    (reduce upper-case-and-conj [] (line-seq rdr))))
 
-(def dict (get-dict "src/capture_words/word_utils/dicts/english.txt"))
+(def all-words (get-all-words "src/capture_words/word_utils/dicts/english.txt"))
 
-dict
+(defn- select-random-letter []
+  "For now, just selects a random letter. Later, should provide
+letters in frequences observed in the language."
+  (str (char (rand-nth (range (int \A) (int \Z))))))
 
+(defn letters []
+  "Lazy sequence of letters with individual frequencies in
+  proproportion to their frequences in the word-list"
+  (seq (repeatedly select-random-letter)))
+
+(defn word? [word]
+  "Inefficient stub for a func that takes a word and returns true or
+  false depending on whether it is a word"
+  (let [upper-word (string/upper-case word)]
+    (boolean (some #{upper-word} all-words))))
