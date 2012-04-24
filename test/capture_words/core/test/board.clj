32d05386118b25bbfa55d223e6d76dacbcee8cf6
Christopher Young <chris@chrisyoung.net>
2012-04-24 09:05:31 -0400

    Rewrote tests using fixture

--- test/capture_words/core/test/board.clj
+++ test/capture_words/core/test/board.clj
@@ -3,23 +3,23 @@
   (:use [capture_words.core.board])
   (:use [clojure.test]))
 
+(defn board-fixture [f]
+  (binding [aboard (atom (make-board))])
+  (f))
+
 (deftest test-neighbors-for-coordinates?
-  (let [board (make-board)]
-    (is (= (neighbors-for-coordinates board [0 0])
-           [[0 1] [1 0]]))
-    (is (= (neighbors-for-coordinates board [4 4])
-           [[4 5] [5 4] [4 3] [3 4]]))))
+  (is (= (neighbors-for-coordinates aboard [0 0])
+         [[0 1] [1 0]]))
+  (is (= (neighbors-for-coordinates aboard [4 4])
+         [[4 5] [5 4] [4 3] [3 4]])))
 
 (deftest test-neighbors?
-  (let [board (make-board)]
-    (is (= (neighbors? board [0 0] [0 1]) true))
-    (is (= (neighbors? board [0 0] [5 5]) false))))
+  (is (= (neighbors? aboard [0 0] [0 1]) true))
+  (is (= (neighbors? aboard [0 0] [5 5]) false)))
 
 (deftest test-change-tile-value
-  (let [board (make-board)
-        changed-board (change-tile-value board [1 1] {:player :test-passer})]
+  (let [changed-board (change-tile-value aboard [1 1] {:player :test-passer})]
     (is (= ((get-tile changed-board [1 1]) :player) :test-passer))))
-
 
 (run-all-tests)
 
