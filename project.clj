29e19c355dfe2c95e873509b636480f87c6dbf0f
Christopher Young <chris@chrisyoung.net>
2012-04-27 10:21:58 -0400

    Added project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject capture_words "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A simple word game."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [org.clojure/math.combinatorics "0.0.2"]]
