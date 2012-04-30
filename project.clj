aba2e32425511b1917a2fa51915a25d2c3969f39
Christopher Young <chris@chrisyoung.net>
2012-04-30 09:44:55 -0400

    Upgrading to lein2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject capture_words "1.0.0-SNAPSHOT"
-  :description "A simple word game."
+(defproject capture_words/capture_words "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [org.clojure/math.combinatorics "0.0.2"]]
-  :dev-dependencies [[midje "1.3.2-SNAPSHOT"]
-                     [lein-midje "1.0.9"]])
+  :profiles {:dev {:dependencies [[midje "1.3.2-SNAPSHOT"]]}}
+  :min-lein-version "2.0.0"
+  :description "A simple word game.")
