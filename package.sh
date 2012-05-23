7450cca2fb8712ea08b05ae6132e88d2836bd4d5
Christopher Young <chris@chrisyoung.net>
2012-05-23 15:48:53 -0400

    Deleted package.sh

--- package.sh
+++
@@ -1,5 +0,0 @@
-
-cd checkouts/sircyb-utils
-lein jar
-cd ../..
-mvn install:install-file -Dfile=checkouts/sircyb-utils/target/sircyb-utils-0.1.0-SNAPSHOT.jar -DcreateChecksum=true -DartifactId=sircyb-utils -Dversion=0.1.0-SNAPSHOT -DgroupId=sircyb-utils -Dpackaging=jar -DlocalRepositoryPath=maven_repo
