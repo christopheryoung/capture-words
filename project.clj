1e35c1f8013ebc558870f634bd35e20d8daa3181
Christopher Young <chris@chrisyoung.net>
2013-04-26 15:05:22 -0400

    Update dependencies and project description

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject capture_words/capture_words "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
                  [org.clojure/math.combinatorics "0.0.2"]
-                 [lein-ring "0.7.0"]
-                 [ring "1.1.0"]
-                 [sircyb-utils "0.1.0-SNAPSHOT"]
-                 [lionel "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+                 [lein-ring "0.8.5"]
+                 [ring "1.1.8"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :plugins [[lein-midje "3.0.0"]]
   :min-lein-version "2.0.0"
-  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repo")))}
   :description "A simple word game.")
